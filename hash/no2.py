c6c887afefa2d93c0756825f9eb5d5169431990a
k-ms1998 <k_ms1998@naver.com>
2021-11-30 10:39:20 +0900

    Hash No2 improving efficiency

--- hash/no2.py
+++ hash/no2.py
@@ -1,20 +1,25 @@
 #phone_book = ["119", "97674223", "1195524421"]
 #phone_book = ["123","456","789"]
 #phone_book = ["12","1235", "123","567","88"]
-#phone_book = ["1235", "123","567","88", "5"]
-phone_book = ["1237", "45", "123"]
+phone_book = ["1235", "123","567","88", "5"]
+#phone_book = ["1237", "45", "123"]
 
 def solution(phone_book):
     answer = True
 
-    pSize = len(phone_book)
-    for i in range(pSize):
-        for j in range(pSize):
-            if i == j or phone_book[j][0] != phone_book[i][0]:
-                continue
-            
-            if phone_book[j][0:len(phone_book[i])] == phone_book[i]:
-                return False  
+    hash = {}
+    for p in phone_book:
+        if p[0] in hash:
+            hash[p[0]].append(p)
+        else:
+            hash[p[0]] = [p]
+
+    for h in hash:
+        if len(hash[h]) > 1:
+            for i in range(len(hash[h])):
+                for j in range(len(hash[h])):
+                    if i != j and (hash[h][j].startswith(hash[h][i]) or hash[h][i].startswith(hash[h][j])):
+                        return False
 
 
     return answer
