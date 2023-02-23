4c8869edb510540be96608cc1ed6635a1af3463f
k-ms1998 <71029015+k-ms1998@users.noreply.github.com>
2023-02-23 15:04:09 +0900

    [unrated] Title: 미로 탈출 명령어, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/unrated/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/unrated/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -9,6 +9,9 @@ class Solution {
     
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
         int dist = Math.abs(x - r) + Math.abs(y - c);
+        /*
+        만약, 현재 위치 부터 도착 위치까지 짝수이면, k의 값도 짝수 일때만 도달 가능 (홀수이면, k가 홀수일때만 도달 가능)
+        */
         if(dist % 2 == k % 2){
             move(n, m, y, x, 0, "", k, c, r);   
         }
@@ -17,6 +20,11 @@ class Solution {
     }
     
     public static void move(int n, int m, int x, int y, int dist, String cur, int target, int dstX, int dstY){
+        /*
+        remainingDist = 앞으로 움직일 수 있는 거리, distFromCurToTarget =  현재 지점부터 탈출 위치까지의 거리
+        앞으로 움직일 수 있는 거리가 현재 지점부터 탈출 위치까지의 거리보다 작으면, 탈출 위치를 도달 X
+        시간 초과를 방지하기 위해서, 도달 하지 못하면 바로 리턴해줌
+        */
         int remainingDist = target - dist;
         int distFromCurToTarget = Math.abs(x - dstX) + Math.abs(y - dstY);
         if(remainingDist < distFromCurToTarget){
