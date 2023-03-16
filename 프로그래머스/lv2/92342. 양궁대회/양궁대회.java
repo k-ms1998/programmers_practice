fa85e6fcc7794cc4bd5888e8d8686262750f948b
k-ms1998 <71029015+k-ms1998@users.noreply.github.com>
2023-03-16 12:24:28 +0900

    [level 2] Title: 양궁대회, Time: 38.75 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
@@ -1,3 +1,13 @@
+/**
+완전탐색/조합론:
+과녁: 0~10 -> 11개, 각 과녁을 최대 맞출수 있는 획수: n (1 <= n <= 10)
+1. 각 점수마다 0~n 개씩 맞출수 있음. 완전 탐색으로 각 점수를 0~n개 맞출수 있는 조합 찾음
+2. 모든 조합에 대해서 점수 계산
+    -> 어피치와 라이언 둘 다 0번 맞춘 점수는 건너뛰고, 똑같은 횟수 만큼 어피치 한테, 그 외에는 더 많은 맞춘 사람한테 점수 부여
+    -> 최종 점수차인 diff보다 크면 최종 조합(ryan) 업데이트
+    -> diff 가 같으면, 더 낮은 점수를 많이 맞춘 조합으로 업데이트
+3. 최종적으로 diff가 0 이면,diff 가 업데이트 된적 없음->라이언이 어피치를 이긴 조합 존재 X->라이언이 어피치를 이길 방법 X
+*/
 class Solution {
     
     static int[] ryan =  new int[11];
@@ -7,23 +17,18 @@ class Solution {
         
         int[] tmp = new int[11];
         findAnswer(0, n, 0, tmp, info);
-        
-        int[] answer;
+
         if(diff == 0){
-            answer = new int[1];
+            int[] answer = new int[1];
             answer[0] = -1;
+            return answer;
         }else{
-            answer = new int[11];
-            for(int i = 0; i < 11; i++){
-                answer[i] = ryan[10 - i];
-            }
+            return ryan;
         }
-        
-        return answer;
     }
     
     public static void findAnswer(int idx, int n, int used, int[] scores, int[] apeach){
-        if(idx == 11 || n == used){
+        if(idx == 11){
             if(n != used){
                 return;
             }
@@ -46,10 +51,10 @@ class Solution {
         int scoreA = 0;
         int scoreR = 0;
         for(int i = 0; i < 11; i++){
-            if(a[i] == 0 && r[10 - i] == 0){
+            if(a[i] == 0 && r[10 - i] == 0){ // 둘 다 0번을 맞췄으면 무시
                 continue;
             }
-            if(a[i] >= r[10 - i]){
+            if(a[i] >= r[10 - i]){ // 어치피는 0번 인덱스가 10점, 라이언은 0번 인덱스가 0점인걸 감안
                 scoreA += (10-i);
             }else{
                 scoreR += (10-i);
@@ -58,24 +63,16 @@ class Solution {
         
         int curDiff = scoreR - scoreA;
         if(diff < curDiff){
-            // for(int i = 0; i < 11; i++){
-            //     System.out.print(r[i] + " ");
-            // }
-            // System.out.println();
-            
             diff = curDiff;
             for(int i = 0; i < 11; i++){
-                ryan[i] = r[i];
+                ryan[10 - i] = r[i];
             }
-        }else if(diff == curDiff){           
-            // for(int i = 0; i < 11; i++){
-            //     System.out.print(r[i] + " ");
-            // }
-            // System.out.println();
+        }else if(diff == curDiff){ //현재 조합과 점수 차이가 같으면, 가장 낮은 점수를 더 많이 맞힌 경우가 최종 정답이 됨
             for(int i = 0; i < 11; i++){
-                if(r[10 - i] > ryan[10 - i]){
+                // r은 0점이 앞에 있음
+                if(r[i] > ryan[10 - i]){
                     for(int j = 0; j < 11; j++){
-                        ryan[j] = r[j];
+                        ryan[10 - j] = r[j];
                     }
                     break;
                 }
