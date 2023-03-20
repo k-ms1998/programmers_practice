d3bdc0089c8c15a9c069862e44284409a82c0349
k-ms1998 <71029015+k-ms1998@users.noreply.github.com>
2023-03-20 09:29:56 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,23 +1,29 @@
 
 import java.util.*;
 
+/**
+(15 분)
+간단 구현
+핵심: 숫자가 문자로 연속될때 판별
+ex: sixseven -> 67
+*/
 class Solution {
 
     static Map<String, Integer> nums = new HashMap<>();
     
     public int solution(String s) {
-        initNums();
+        initNums(); //  문자랑 숫자를 매핑해주기
         
         String tmp = "";
         int answer = 0;
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            if(c >= '0' && c <= '9'){
+            if(c >= '0' && c <= '9'){ // 현재 문자가 숫자이면 그대로 정답에 추가
                 answer = 10*answer + Integer.parseInt(String.valueOf(c));
             }else{
-                tmp += String.valueOf(c);
+                tmp += String.valueOf(c); // 알파벳이면 tmp에 추가
             }
-            if(nums.containsKey(tmp)){
+            if(nums.containsKey(tmp)){ // tmp가 nums에 key값이 존재할때마다 숫자로 변환해서 정답에 추가 -> 연속되다리도 판별할수 있음; ex: sixseven에면 tmp가 "six"이면 6이 추가되고 tmp는 초기화 됨 -> 다시 seven을 독립적으로 넣을 수 있음
                 answer = 10 * answer + nums.get(tmp);
                 tmp = "";
             }
