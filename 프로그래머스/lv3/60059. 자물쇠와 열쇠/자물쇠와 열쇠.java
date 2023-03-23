dc8085ce2d5327f0fab2612beaf1c987e1eb7717
k-ms1998 <71029015+k-ms1998@users.noreply.github.com>
2023-03-23 14:21:09 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.98 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,11 +1,24 @@
 import java.util.*;
 
+/**
+n x n 배열 회전시키기
+-> 시계 방향:  ny = x & nx = (n - 1) - y
+-> 반시계 방향: nx = y & ny = (n - 1) - x
+    
+1. lock의 3배 크기인 map 생성
+2. map의 중앙에 lock 배치
+3. key를 시계방향으로 회전
+4. 회전된 key를 map의 (0,0) 부터 시작해서 배치
+5. lock이 저장되어 있는 부분 확인
+6. lock이 저장되어 있는 부분이 모두 1이면, 자물쇠와 열쇠가 딱 맞게 만나는 부문. 아닐 경우, 3~6 반복
+    (0이면, 열쇠가 자물쇠를 채우지 못한 부분, 2이면 열쇠의 돌출된 부분과 자물쇠의 돌출된 부분이 맞닿는 부분)
+
+*/
 class Solution {
     
     static int n, m;
     static boolean answer = false;
     static int[][] map;
-    static int flag = 0;
     
     public boolean solution(int[][] key, int[][] lock) {     
         n = key.length;
@@ -14,12 +27,11 @@ class Solution {
         map = new int[3*m][3*m];
         for(int y = 0; y < m; y++){
             for(int x = 0; x < m; x++){
-                map[y + m][x + m] = lock[y][x]; 
+                map[y + m][x + m] = lock[y][x]; // map의 중간부분에 자물쇠 배치
             }
         }
 
         for(int i = 0; i < 4; i++){     
-            flag = i;
             findAnswer(key);
             if(answer){
                 break;
@@ -35,16 +47,6 @@ class Solution {
             for(int x = 0; x < (3*m - n); x++){
                 updateMap(key, x, y);
                 boolean cur = checkMap();        
-                // if(flag == 1){
-                //     for(int yy = 0; yy < 3*n; yy++){
-                //         for(int xx = 0; xx < 3*m; xx++){
-                //             System.out.print(map[yy][xx] + " ");
-                //         }
-                //         System.out.println();
-                //     }
-                //     System.out.println("=========");
-                // }
-
                 if(cur){
                     answer = true;
                     return;
@@ -82,6 +84,7 @@ class Solution {
         }
     }
     
+    // 시계 방향 회전
     public static int[][] rotateClockWise(int[][] key){
         int[][] tmp = new int[n][n];
         
@@ -96,6 +99,7 @@ class Solution {
         return tmp;
     }
     
+    // 반시계 방향 회전
     public static int[][] rotateAntiClockWise(int[][] key){
         int[][] tmp = new int[n][n];
         
@@ -107,16 +111,6 @@ class Solution {
             }
         }
         return tmp;
-    }
-    
-    public static void printGrid(int[][] grid){
-        for(int y = 0; y < grid.length; y++){
-            for(int x = 0; x < grid[0].length; x++){
-                System.out.print(grid[y][x] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("----------");
     }
 
 }
