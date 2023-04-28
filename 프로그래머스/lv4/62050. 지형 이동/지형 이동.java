7d54979adcf7ab62a37be434a1ebc08d265d73e7
김민섭 <71029015+k-ms1998@users.noreply.github.com>
2023-04-28 16:22:50 +0900

    [level 4] Title: 지형 이동, Time: 257.26 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/lv4/62050. 지형 이동/지형 이동.java
+++ 프로그래머스/lv4/62050. 지형 이동/지형 이동.java
@@ -1,5 +1,23 @@
 import java.util.*;
 
+/**
+Solution: Flood + Kruskal + Union-Find
+=> 사다리를 놓지 않고도 움직일 수 있으면 비용 증가 X => 그러므로, 사다리를 놓는 비용만 생각하면 됨
+    => 섬 문제와 비슷: 섬들끼리 모두 도달 가능한 최소 비용만 계산하면 됨 -> 섬들끼리 연결하는데 비용 계산
+
+1. (0,0) 부터 시작해서 모든 좌표 탐색
+2. 각 좌표로 부터, 사다리를 놓지 않고 도달할 수 있는 좌표들끼리 묶고 (Flood), 해당 묶음에 속해 있는 모든 점들 저장
+3. 각 섬들에 속한 모든 좌표들 확인
+    -> 각 좌표보다 속해 있는 모든 좌표 확인후, 다른 묶음과 인접해 있는 점들 찾기
+        -> 두 좌표간의 간선 저장
+            -> 간선 = 하나의 묶음으로 부터 인접해 있는 묶으로 가는데 드는 비용
+                -> a =  하나의 묶음, b =  다른 하나의 묶음, dist = 두 좌표 사이에 사다리를 놓는 비용
+4. 간선들을 가중치가 낮은순으로 정렬
+5. 가중치가 낮은 간선들을 순서대로 탐색(Kruskal)
+6. 이미 연결되어 있는지 확인(Union-Find)
+    -> 연결이 안되어 있으면 간선 추가 -> answer 업데이트 -> answer = answer + dist;
+
+*/
 class Solution {
     
     static int n;
@@ -8,7 +26,6 @@ class Solution {
     static int[] dy = {1, 0, -1, 0};
     static final int INF = 100000000;
     static Map<Integer, List<Point>> lists = new HashMap<>();
-    static Map<Integer, Integer> count = new HashMap<>();
     static int[] parent;
     
     public int solution(int[][] land, int height) {
@@ -19,6 +36,7 @@ class Solution {
         int num = 0;
         for(int y = 0; y < n; y++){
             for(int x = 0; x < n; x++){
+                // 각 섬들끼리 묶기
                 if(island[y][x] == 0){
                     num++;
                     island[y][x] = num;
@@ -52,31 +70,19 @@ class Solution {
                         }
                     }
                     
-                    lists.put(num, list);
+                    lists.put(num, list); // 각 섬마다 해당 섬에 속해 있는 모든 좌표들 확인하기
                 }
             }
         }
         
-//         for(int y= 0 ; y < n; y++){
-//             for(int x = 0; x < n; x++){
-//                 System.out.print(island[y][x]);
-//             }
-//             System.out.println();
-//         }
-        // System.out.println("lists" + lists);
-        
-        PriorityQueue<Edge> edges = new PriorityQueue<>(new Comparator<Edge>(){
-            @Override
-            public int compare(Edge e1, Edge e2){
-                return e1.dist - e2.dist;
-            }
-        });
+        // 간선들 생성하기
+        List<Edge> edges = new ArrayList<>();
         parent = new int[num + 1];
         for(int i = 1; i <= num; i++){
             parent[i] = i;
             List<Point> list = lists.get(i);
-            for(Point p : list){
-                for(int  j = 0; j < 4; j++){
+            for(Point p : list){ // 현재 섬에 속한 모든 점들 확인
+                for(int  j = 0; j < 4; j++){ // 각 점마다 인접한 노드 확인
                     int nx = p.x + dx[j];
                     int ny = p.y + dy[j];
                     
@@ -84,7 +90,7 @@ class Solution {
                         continue;
                     }
                     
-                    if(island[ny][nx] != i){
+                    if(island[ny][nx] != i){ //인접한 점이 현재 점이랑 다른 섬이면 간성 생성
                         int diff = Math.abs(land[ny][nx] - land[p.y][p.x]);
                         if(diff > height){
                             int a = Math.min(i, island[ny][nx]);
@@ -96,9 +102,14 @@ class Solution {
                 }
             }
         }
-        // System.out.println("edges" + edges);
-        while(!edges.isEmpty()){
-            Edge edge = edges.poll();
+
+        Collections.sort(edges, new Comparator<Edge>(){
+            @Override
+            public int compare(Edge e1, Edge e2){
+                return e1.dist - e2.dist;
+            }
+        });
+        for(Edge edge: edges){ // 가중치가 낮은 순으로 모든 간선 확인
             int a = edge.a;
             int b = edge.b;
             int dist = edge.dist;
@@ -106,13 +117,12 @@ class Solution {
             int rootA = findRoot(a);
             int rootB = findRoot(b);
             
-            if(rootA != rootB){
+            if(rootA != rootB){ // 이미 서로 연결되어 있는 상태가 아니면 연결 시키기
                 parent[b] = rootA;
                 parent[rootB] = rootA;
                 answer += dist;
             }
         }
-
         
         return answer;
     }
