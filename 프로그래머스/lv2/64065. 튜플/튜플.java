816728b28a5bf878dee3d2d4b3485bddc303caad
김민섭 <71029015+k-ms1998@users.noreply.github.com>
2023-05-16 09:22:34 +0900

    [level 2] Title: 튜플, Time: 2.55 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     
-    static Set<Integer> set = new HashSet<>();
+    static boolean[] used;
     
     public int[] solution(String s) {
         PriorityQueue<List<Integer>> queue = new PriorityQueue<>(new Comparator<List<Integer>>(){
@@ -10,38 +10,41 @@ class Solution {
             public int compare(List<Integer> l1, List<Integer> l2){
                 return l1.size() - l2.size();
             }
-        });
+        }); // 튜플들을 크기가 작은 순으로 정렬
         
-        String tmp = "";
+        int maxNum = 0;
+        String tmp = ""; // 현재 튜플을String으로 저장
         boolean flag = false;
         for(int i = 1; i < s.length() - 1; i++){
             char c = s.charAt(i);
             if(c == '{'){
-                flag = true;
-            }else if(c == '}'){
+                flag = true; // 튜플의 시작을 알려줌
+            }else if(c == '}'){ // 튜플의 끝을 알려줌
                 List<Integer> list = new ArrayList<>();
-                String[] arr = tmp.split(",");
+                String[] arr = tmp.split(","); // 튜플 안에 있는 원소들 가져오기
                 for(int j = 0; j < arr.length; j++){
+                    maxNum = Math.max(maxNum, Integer.parseInt(arr[j]));
                     list.add(Integer.parseInt(arr[j]));
                 }
                 queue.offer(list);
                 tmp = "";
                 flag = false;
             }else{
-                if(c == ',' && !flag){
+                if(!flag){
                     continue;
                 }
                 tmp += String.valueOf(c);
             }
         }
 
+        used = new boolean[maxNum + 1];
         List<Integer> ansList = new ArrayList<>();
         while(!queue.isEmpty()){
             List<Integer> list = queue.poll();
             for(int num : list){
-                if(!set.contains(num)){
+                if(!used[num]){
                     ansList.add(num);
-                    set.add(num);
+                    used[num] = true;
                 }
             }
         }
