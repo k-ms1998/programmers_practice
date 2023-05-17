8e303f5de811f829bc2f8bec2b1f5766a1002965
김민섭 <71029015+k-ms1998@users.noreply.github.com>
2023-05-17 11:03:47 +0900

    [level 3] Title: 징검다리 건너기, Time: 51.88 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,10 +1,15 @@
 import java.util.*;
 
+/**
+Solution: Sliding Window
+->ex:
+=> stones = [2, 4, 5, 3, 2, 1, 4, 2, 5, 1], k = 3일때, 3개씩 묶어서 가장 큰 값들 중에서, 작은 값이 정답
+=> (2, 4, 5), (4, 5, 3), (5, 3, 2), (3, 2, 1), (2, 1, 4), (1, 4, 2), (4, 2, 5), (2, 5, 1)
+=> 5, 5, 5, 3, 4, 4, 5, 5
+=> Answer = 3
+*/
 class Solution {
-    
-    static Map<Integer, Integer> map = new HashMap<>();
-    static int answer = 0;
-    
+        
     public int solution(int[] stones, int k) {
         PriorityQueue<Node> queue = new PriorityQueue<>(new Comparator<Node>(){
             @Override
@@ -13,6 +18,7 @@ class Solution {
             }
         });
         
+        int answer = 0;
         for(int i = 0; i < k; i++){
             queue.offer(new Node(i, stones[i]));
             answer = Math.max(answer, stones[i]);
@@ -20,8 +26,8 @@ class Solution {
         for(int i = k; i < stones.length; i++){
             queue.offer(new Node(i, stones[i]));
             while(true){
-                Node node = queue.peek();
-                if(node.idx > i - k && node.idx <= i){
+                Node node = queue.peek(); // 가장 큰 값
+                if(node.idx > i - k && node.idx <= i){ // 노드가 현재 탐색 중인 범위 안에 있을때만 확인; 범위 밖이면 pop
                     break;
                 }
                 queue.poll();
