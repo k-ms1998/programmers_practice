f5eea9c6edc8047fe137e64ffe32857263c4f821
김민섭 <71029015+k-ms1998@users.noreply.github.com>
2023-05-26 09:24:42 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 1.86 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,72 +1,49 @@
 class Solution {
     
-    static int[] scores = {0, 3, 2, 1, 0, 1, 2, 3};
-    static int[][] totalScores = new int[4][2];
+    static int n;
+    static int[] index = {0, -3, -2, -1, 0, 1, 2, 3};
+    static int[] scores =  new int[4];
+    static String[][] map = {{"R", "T"}, {"C", "F"}, {"J", "M"}, {"A", "N"}};
     
     public String solution(String[] survey, int[] choices) {
-        String answer = "";
-        
-        int n = survey.length;
+        n = survey.length;
+            
         for(int i = 0; i < n; i++){
-            String[] tmp = survey[i].split("");
-            int score = scores[choices[i]];
+            String s = survey[i];
+            int choice = choices[i];
+            int score = index[choice];
             
-            String idxStr = choices[i] <= 3 ? tmp[0] : tmp[1];
-            int idxA = getIdx(idxStr);
-            int idxB;
-            if(idxStr.equals("R") || idxStr.equals("C") || idxStr.equals("J") || idxStr.equals("A")){
-                idxB = 0;
-            }else{
-                idxB = 1;
+            String a = String.valueOf(s.charAt(0));
+            String b = String.valueOf(s.charAt(1));
+            if(a.equals("R")){
+                scores[0] += score;
+            }else if(a.equals("T")){
+                scores[0] -= score;
+            }else if(a.equals("C")){
+                scores[1] += score;
+            }else if(a.equals("F")){
+                scores[1] -= score;
+            }else if(a.equals("J")){
+                scores[2] += score;
+            }else if(a.equals("M")){
+                scores[2] -= score;
+            }else if(a.equals("A")){
+                scores[3] += score;
+            }else if(a.equals("N")){
+                scores[3] -= score;
             }
-            
-            totalScores[idxA][idxB] += score;
-            
+        }
+                
+        String answer = "";
+        for(int i = 0; i < 4; i++){
+            if(scores[i] <= 0){
+                answer += map[i][0];
+            }else{
+                answer += map[i][1];
+            }
         }
         
-        for(int i = 0; i < 4; i++){
-            // System.out.println(totalScores[i][0] + ", " + totalScores[i][1]);
-            int scoreA = totalScores[i][0];
-            int scoreB = totalScores[i][1];
-            if(scoreA > scoreB){
-                answer += getString(i, 0);
-            }else if(scoreA < scoreB){
-                answer += getString(i, 1);
-            }else{
-                String strA = getString(i, 0);
-                String strB = getString(i, 1);
-                if(strA.compareTo(strB) < 0){
-                    answer += strA;
-                }else{
-                    answer += strB;
-                }
-            }
-        }
         
         return answer;
-    }
-    
-    public static String getString(int idxA, int idxB){
-        if(idxA == 0){
-            return idxB == 0 ? "R" : "T";
-        }else if(idxA == 1){
-            return idxB == 0 ? "C" : "F";
-        }else if(idxA == 2){
-            return idxB == 0 ? "J" : "M";
-        }else{
-            return idxB == 0 ? "A" : "N";
-        }
-    }
-    
-    public static int getIdx(String s){
-        if(s.equals("R") || s.equals("T")){
-            return 0;
-        }else if(s.equals("C") || s.equals("F")){
-            return 1;
-        }else if(s.equals("J") || s.equals("M")){
-            return 2;
-        }else{
-            return 3;
-        }
     }
 }
