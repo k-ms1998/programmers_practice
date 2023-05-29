d7c47f5f49ec29ae99abe5c6b81cadeed872b598
김민섭 <71029015+k-ms1998@users.noreply.github.com>
2023-05-29 19:33:08 +0900

    [level 3] Title: 합승 택시 요금, Time: 10.45 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,65 +1,70 @@
-/*
-Floyd-Warshall
-1. 각 노드간의 최단거리를 알고 있어야 하기 때문에 floyd로 찾아줌 (3 <= n <= 200 , 27 <= n^3 <= 8,000,000)
-2. 이때, 같이 합승을 하다가 중간에 각자 갈 수 있는 것이 중요 => (s->i) + (i->a) + (i->b)
-    처음부터 끝까지 같이 하는게 제일 적은 비용이 들수도 있음 => (s->a) + (a->b) OR (s->b) + (b->a)
-    처음부터 각자 따로 가는게 더 제일 적은 비용이 들수도 있음 => (s->a) + (s->b)
-*/
+import java.util.*;
+
 class Solution {
     
+    static List<Edge>[] edges;
     static int[][] dist;
     static final int INF = 100000000;
     
     public int solution(int n, int s, int a, int b, int[][] fares) {
+        edges = new List[n + 1];
         dist = new int[n + 1][n + 1];
-        for(int i = 1; i < n + 1; i++){
-            for(int j = 1; j < n + 1; j++){
-                dist[i][j] = INF;
-            }
+        for(int i = 0; i < n + 1; i++){
+            edges[i] = new ArrayList<>();
+            Arrays.fill(dist[i], INF);
         }
         
         for(int i = 0; i < fares.length; i++){
-            int n1 = fares[i][0];
-            int n2 = fares[i][1];
-            int f = fares[i][2];
+            int src = fares[i][0];
+            int dst = fares[i][1];
+            int cost = fares[i][2];
             
-            dist[n1][n2] = f;
-            dist[n2][n1] = f;
+            edges[src].add(new Edge(dst, cost));
+            edges[dst].add(new Edge(src, cost));
+            
+            dist[src][dst] = cost;
+            dist[dst][src] = cost;
+            dist[src][src] = 0;
+            dist[dst][dst] = 0;
         }
-        floyd(n);
         
-        int answer = INF;
-        answer = Math.min(dist[s][a] + dist[s][b], Math.min(dist[s][a] + dist[a][b], dist[s][b] + dist[b][a]));
+        initDist(n);
+        int dist1 = dist[s][a] + dist[s][b];
+        int dist2 = INF;
         for(int i = 1; i < n + 1; i++){
-            if(i == s || dist[s][i] >= INF){
+            if(i == s || dist[s][i] == INF){
                 continue;
-            }else if(i == a && dist[i][b] < INF){
-                answer = Math.min(answer, dist[s][i] + dist[i][b]);
-            }else if(i == b && dist[i][a] < INF){
-                answer = Math.min(answer, dist[s][i] + dist[i][a]);
-            }else{
-                if(dist[i][a]  < INF && dist[i][b] < INF){
-                    answer = Math.min(answer, dist[s][i] + dist[i][a] + dist[i][b]);
-                }
             }
+            dist2 = Math.min(dist2, dist[s][i] + dist[i][a] + dist[i][b]);
         }
+        // System.out.println("dist1=" + dist1 + ", dist2=" + dist2);
         
-        return answer;
+        return Math.min(dist1, dist2);
     }
     
-    public static void floyd(int n){
+    public static void initDist(int n){
         for(int k = 1; k < n + 1; k++){
             for(int i = 1; i < n + 1; i++){
-                for(int j = i + 1; j < n + 1; j++){
-                    if(dist[i][k] == INF || dist[k][j] == INF || i == k || j == k || i == j){
+                for(int j = 1; j < n + 1; j++){
+                    if(i == k || j == k || i == j){
                         continue;
                     }
                     
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[j][k]);
-                    dist[j][i] = dist[i][j];
+                    if(dist[i][k] != INF && dist[k][j] != INF){
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
                 }
             }
         }
+    }
+    
+    public static class Edge{
+        int dst;
+        int cost;
         
+        public Edge(int dst, int cost){
+            this.dst = dst;
+            this.cost = cost;
+        }
     }
 }
