9a152f804a694c0d808f55ecb1543a8c35a7cec4
김민섭 <71029015+k-ms1998@users.noreply.github.com>
2023-09-05 22:36:01 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 3.56 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,6 +1,23 @@
 import java.io.*;
 import java.util.*;
 
+/**
+Solution: BruteForce
+모든 경우의 수를 생각해서 정답 구하기
+But, 시간초과를 방지하기 위해 visited를 통해 이미 확인한 경우의 수이면 건더뛰기
+-> visited[r][down][right][up][left] = r번 회전시키고, 밑으로 d번, 오른쪽으로 r번, 위쪽으로 u번 그리고 왼쪽으로 l번 움지였을때의 상태 
+
+열쇠와 자물쇠가 맞는지 확인하기:
+1. 자물쇠의 홈들의 위치를 구하기
+-> (0,0) 부터 시작해서 가장 첫번째 홈 구하기
+    -> 해당 홈을 기준으로 시작해서 다른 홈들은 상대적으로 어디에 있는지 확인해서 리스트에 저장하기
+2. 각 상태의 열쇠에 대해서 돌기 부분 구하기
+-> (0,0) 부터 시작해서 가장 첫번째 돌기 구하기
+    -> 해당 돌기를 기준으로 시작해서 다른 돌기들은 상대적으로 어디에 있는지 확인해서 리스트에 저장하기
+3. 그렇게 구한 좌표들을 x가 작은 순서, y가 작은 순서로 정렬
+4. 두 리스트들을 앞에서 부터 순서대로 확인해서 모든 값을 정확히 일치하는지 확인하기
+-> 정확히 일치하면 해당 열쇠를 자물쇠를 열 수 있는 것
+*/
 class Solution {
     
     static boolean answer = false;
@@ -9,7 +26,7 @@ class Solution {
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
     static boolean[][][][][] visited;
-    // down, right, up, left
+    // rotate, down, right, up, left
     
     public boolean solution(int[][] key, int[][] lock) {
         n = lock.length;
